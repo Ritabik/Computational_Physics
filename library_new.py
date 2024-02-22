1aef86838403e8de5cb9874d5d7cf8f5bb168950
Ritabik <ritabik.banerjee@niser.ac.in>
2024-02-22 15:35:19 +0530

    change

--- library_new.py
+++ library_new.py
@@ -94,5 +94,113 @@ def crank_nicolson_solver(L, T, Nx, Nt, alpha, initial_condition, boundary_condi
 
     return x_values, t_values, u
 
+def regula_falsi(f, a, b, tol=1e-6, max_iter=1000):
+    for i in range(max_iter):
+        fa = f(a)
+        fb = f(b)
+        x_next = (a * fb - b * fa) / (fb - fa)
+        
+        if abs(f(x_next)) < tol:
+            return x_next, i + 1  # Return the root and the number of iterations
+        
+        if f(a) * f(x_next) < 0:
+            b = x_next
+        else:
+            a = x_next
+    
+    return None, max_iter  # Return None if the method does not converge within max_iter iterations
+
+def newton_raphson(f, f_prime, x0, tol=1e-6, max_iter=1000):
+    x = x0
+    for i in range(max_iter):
+        fx = f(x)
+        fpx = f_prime(x)
+        x = x - fx / fpx
+        
+        if abs(fx) < tol:
+            return x, i + 1  # Return the root and the number of iterations
+    
+    return None, max_iter  # Return None if the method does not converge within max_iter iterations
+
+def Lagrange_interpol(zeta_h, zeta_l, yh, yl, y):    
+                                    
+    zeta = zeta_l + (zeta_h - zeta_l) * (y - yl)/(yh - yl)
+    return zeta
+def RK_shooting(Func_d2ydx2, func_dydx, x0, y0, z0, xf, h):      
+
+    x = [x0]
+    y = [y0]
+    z = [z0]
+    N = int((xf-x0)/h)
+
+    for i in range(N): 
+
+        k1 = h * func_dydx(x[i], y[i], z[i])
+        l1 = h * Func_d2ydx2(x[i], y[i], z[i])
+        
+        k2 = h * func_dydx(x[i] + h/2, y[i] + k1/2, z[i] + l1/2)
+        l2 = h * Func_d2ydx2(x[i] + h/2, y[i] + k1/2, z[i] + l1/2)
+        
+        k3 = h * func_dydx(x[i] + h/2, y[i] + k2/2, z[i] + l2/2)
+        l3 = h * Func_d2ydx2(x[i] + h/2, y[i] + k2/2, z[i] + l2/2)
+        
+        k4 = h * func_dydx(x[i] + h, y[i] + k3, z[i] + l3)
+        l4 = h * Func_d2ydx2(x[i] + h, y[i] + k3, z[i] + l3)
+        
+        x.append(x[i] + h)
+        y.append(y[i] + (k1 + 2*k2 + 2*k3 + k4)/6)
+        z.append(z[i] + (l1 + 2*l2 + 2*l3 + l4)/6)
+
+    return x, y, z
+
+def RKshooting_method(Func_d2ydx2, func_dydx, x0, y0, xf, yf, z1, z2, h, tol=1e-6):  
+                   
+    x, y, z = RK_shooting(Func_d2ydx2, func_dydx, x0, y0, z1, xf, h)
+
+    yn = y[-1]
+
+    if abs(yn - yf) > tol:
+
+        if yn < yf:
+
+            zeta_l = z1
+            yl = yn
+            x, y, z = RK_shooting(Func_d2ydx2, func_dydx, x0, y0, z2, xf, h)
+            yn = y[-1]
+
+            if yn > yf:
+
+                zeta_h = z2
+                yh = yn
+                zeta = Lagrange_interpol(zeta_h, zeta_l, yh, yl, yf)
+                x, y, z = RK_shooting(Func_d2ydx2, func_dydx, x0, y0, zeta, xf, h)
+                return x, y
+            
+            else:
+
+                print("Invalid bracketing.")
+
+        elif yn > yf:
+
+            zeta_h = z1
+            yh = yn
+            x, y, z = RK_shooting(Func_d2ydx2, func_dydx, x0, y0, z2, xf, h)
+            yn = y[-1]
+
+            if yn < yf:
+
+                zeta_l = z2
+                yl = yn
+                zeta = Lagrange_interpol(zeta_h, zeta_l, yh, yl, yf)
+                x, y, z = RK_shooting(Func_d2ydx2, func_dydx, x0, y0, zeta, xf, h)
+                return x, y
+            
+            else:
+
+                print("Invalid bracketig.")
+    else:
+
+        return x, y
+
 
 
